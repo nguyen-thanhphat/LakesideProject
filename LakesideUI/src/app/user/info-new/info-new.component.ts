75d758161484f906a93201799ff2d475be480b3b
Thanh Phat <62296306+nguyen-thanhphat@users.noreply.github.com>
2023-05-17 15:46:16 +0700

    UI-add-introduce

---
+++ LakesideUI/src/app/user/info-new/info-new.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-info-new',
+  templateUrl: './info-new.component.html',
+  styleUrls: ['./info-new.component.css']
+})
+export class InfoNewComponent {
+
+}
--- LakesideUI/src/app/user/user.module.ts
+++ LakesideUI/src/app/user/user.module.ts
@@ -4,12 +4,18 @@ import { CommonModule } from '@angular/common';
 import { UserRoutingModule } from './user-routing.module';
 import { HomepageComponent } from './homepage/homepage.component';
 import { MainComponent } from './main/main.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { InfoNewComponent } from './info-new/info-new.component';
+import { IntroduceComponent } from './introduce/introduce.component';
 
 
 @NgModule({
   declarations: [
     HomepageComponent,
-    MainComponent
+    MainComponent,
+    FeedbackComponent,
+    InfoNewComponent,
+    IntroduceComponent
   ],
   imports: [
     CommonModule,
