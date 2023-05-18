3e03ee76f688896b0230cb1af18e63583b2006f6
Thanh Phat <62296306+nguyen-thanhphat@users.noreply.github.com>
2023-05-18 09:54:58 +0700

    format-infoBooking

--- LakesideUI/src/app/user/info-booking/info-booking.component.ts
+++ LakesideUI/src/app/user/info-booking/info-booking.component.ts
@@ -7,7 +7,7 @@ import { environment } from 'src/environment/environment';
 @Component({
   selector: 'app-info-booking',
   templateUrl: './info-booking.component.html',
-  styleUrls: ['./info-booking.component.css']
+  styleUrls: ['./info-booking.component.css'],
 })
 export class InfoBookingComponent implements OnInit {
   searchResult: any;
@@ -19,8 +19,8 @@ export class InfoBookingComponent implements OnInit {
     soDinhDanh: '',
     ngayNhan: '',
     ngayTra: '',
-    maPhong: 0
-  }
+    maPhong: 0,
+  };
 
   checkIn: string = '';
   checkOut: string = '';
@@ -29,26 +29,22 @@ export class InfoBookingComponent implements OnInit {
 
   totalPrice: number = 0;
 
-  constructor(
-    private route: ActivatedRoute,
-    private http: HttpClient
-  ) {}
+  constructor(private route: ActivatedRoute, private http: HttpClient) {}
 
   ngOnInit(): void {
-      this.searchResult = JSON.parse(
-        this.route.snapshot.paramMap.get('searchResult') || '{}'
-      );
-      this.days = parseInt(this.route.snapshot.paramMap.get('days') || '0', 10);
-      this.checkIn = this.route.snapshot.paramMap.get('checkIn') || '';
-      this.checkOut = this.route.snapshot.paramMap.get('checkOut') || '';
-      this.totalPrice = this.searchResult.giaPhong * this.days;
+    this.searchResult = JSON.parse(
+      this.route.snapshot.paramMap.get('searchResult') || '{}'
+    );
+    this.days = parseInt(this.route.snapshot.paramMap.get('days') || '0', 10);
+    this.checkIn = this.route.snapshot.paramMap.get('checkIn') || '';
+    this.checkOut = this.route.snapshot.paramMap.get('checkOut') || '';
+    this.totalPrice = this.searchResult.giaPhong * this.days;
 
-
-      this.formBooking.maPhong = this.searchResult.maPhong;
-      this.formBooking.ngayNhan = this.checkIn;
-      this.formBooking.ngayTra = this.checkOut;
-      console.log(this.searchResult);
-    };
+    this.formBooking.maPhong = this.searchResult.maPhong;
+    this.formBooking.ngayNhan = this.checkIn;
+    this.formBooking.ngayTra = this.checkOut;
+    console.log(this.searchResult);
+  }
 
   // postData(){
   //   this.formBooking.ngayNhan = this.checkIn;
@@ -61,12 +57,23 @@ export class InfoBookingComponent implements OnInit {
     this.formBooking.ngayNhan = this.checkIn;
     this.formBooking.ngayTra = this.checkOut;
     this.formBooking.maPhong = this.searchResult.maPhong;
-    this.http.post(`${environment.apiUrl}reservations/create`, this.formBooking)
+    if (
+      !this.formBooking.tenKhachHang ||
+      !this.formBooking.soDienThoai ||
+      !this.formBooking.email ||
+      !this.formBooking.soDinhDanh
+    ) {
+      // Kiểm tra xem các trường bắt buộc đã được nhập hay chưa
+      alert('Vui lòng điền đầy đủ thông tin.'); // Hoặc hiển thị thông báo lỗi khác
+      return;
+    }
+    this.http
+      .post(`${environment.apiUrl}reservations/create`, this.formBooking)
       .subscribe(
-        response => {
+        (response: any) => {
           console.log(response);
         },
-        error => {
+        (error) => {
           console.log(error);
         }
       );
