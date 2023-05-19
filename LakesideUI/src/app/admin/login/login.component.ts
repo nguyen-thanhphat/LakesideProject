847a5a19c5b8620c59941d5a23f09d8c50a380db
Thanh Phat <62296306+nguyen-thanhphat@users.noreply.github.com>
2023-05-19 10:24:10 +0700

    change-template-login

--- LakesideUI/src/app/admin/login/login.component.ts
+++ LakesideUI/src/app/admin/login/login.component.ts
@@ -4,26 +4,8 @@ import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  template: `
-    <h2>Login</h2>
-    <form (ngSubmit)="login()">
-      <div>
-        <label for="username">Username:</label>
-        <input type="text" id="username" [(ngModel)]="username" name="username" required>
-      </div>
-      <div>
-        <label for="password">Password:</label>
-        <input type="password" id="password" [(ngModel)]="password" name="password" required>
-      </div>
-      <button type="submit">Login</button>
-    </form>
-    <p *ngIf="loginError" class="error-message">Invalid username or password</p>
-  `,
-  styles: [`
-    .error-message {
-      color: red;
-    }
-  `]
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
   username: string = '';
